319ce23f9bbcb61acadbb976ee9484888e970fd0
Minamic981 <serial913.abuii@gmail.com>
2025-01-21 11:34:54 +0330

    Update routes.py
    WTF I got confused with js. comment

--- routes/routes.py
+++ routes/routes.py
@@ -5,7 +5,7 @@ from routes import routes_bp
 def index():
     return render_template("index.html")
 
-//Todo Must Be Changed When Done easteregg.html
+#Todo Must Be Changed When Done easteregg.html
 @routes_bp.route("/easteregg")
 def easter_egg():
     return render_template("")
