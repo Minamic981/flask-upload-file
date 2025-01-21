d8ea6a4a7da25938968f3dafb8c3311540df718b
Minamic981 <serial913.abuii@gmail.com>
2025-01-21 11:37:56 +0330

    I am drowning in drugs.

--- routes/example.py
+++ routes/example.py
@@ -2,6 +2,6 @@ from routes import routes_bp
 from flask import render_template
 from services.s3_service import s3_client, BUCKET_NAME
 
-@route_bp.route('/example')
+@routes_bp.route('/example')
 def example():
     return render_template("example.html")
