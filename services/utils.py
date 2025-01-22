2d6e7f3ca74fb7ee947af72045596e7068eea734
Minamic981 <serial913.abuii@gmail.com>
2025-01-22 07:10:24 -0500

    add better test-

--- services/utils.py
+++ services/utils.py
@@ -11,7 +11,7 @@ def is_valid_url(url):
     if not url:
         return False
     if url.startswith('test-'):
-        return 'https://www.google.com'
+        return url[5:]
     regex = re.compile(
         r"^(?:http|ftp)s?://"  # http:// or https://
         r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain
