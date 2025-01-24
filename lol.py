d4f1921216e44f79d42092320c1f57e389406448
Minamic981 <serial913.abuii@gmail.com>
2025-01-24 17:18:37 -0500

    i add a countdown

--- lol.py
+++ lol.py
@@ -1,3 +1,10 @@
-import sys
+def generate_file_low_cpu_optimized(size_mb, file_path):
+    base_chunk = b'a' * 10
+    block = base_chunk * 1024
+    repetitions = (size_mb * 1024 * 1024) // len(block)
 
-print(sys.argv)
+    with open(file_path, 'wb') as f:
+        for _ in range(repetitions):
+            f.write(block)
+            
+generate_file_low_cpu_optimized(300, '/tmp/o.txt')
--- send_to_s3.py
+++ send_to_s3.py
@@ -1,26 +1,32 @@
 import requests
 import boto3
 from botocore.config import Config
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request as reqf
+from dotenv import load_dotenv
 import os
+from os import getenv as env
 app = Flask(__name__)
+load_dotenv()
+endpoint_url = env('ENDPOINT_URL', 'https://n1d2.fra202.idrivee2-98.com')
+aws_access_key_id = env('AWS_ACCESS_KEY_ID','apELPELYSuMIfC4GMu0v')
+aws_secret_access_key = env('AWS_SECRET_ACCESS_KEY', 'QIdUzrinsZkEYnAUYVRjgucpG2OWkdiKh7G3X2m3')
+BUCKET_NAME = env('AWS_BUCKET_NAME', 'uploads')
 s3_client = boto3.client('s3',
-                         endpoint_url="https://n1d2.fra202.idrivee2-98.com",
-                         aws_access_key_id="apELPELYSuMIfC4GMu0v",
-                         aws_secret_access_key="QIdUzrinsZkEYnAUYVRjgucpG2OWkdiKh7G3X2m3",
-                         config=Config(signature_version='s3v4')
-                         )
+                        endpoint_url=endpoint_url,
+                        aws_access_key_id=aws_access_key_id,
+                        aws_secret_access_key=aws_secret_access_key,
+                        config=Config(signature_version='s3v4')
+                        )
 
-def generate_10mb_file():
-    file_path = '/tmp/10mb_file.txt'
-    file_size_mb = 10  # Size of the file in MB
-    chunk_size = 1024 * 1024  # 1MB
+def generate_file(size_mb):
+    file_path = f'/tmp/{size_mb}.txt'
+    base_chunk = b'a' * 10
+    block = base_chunk * 1024
+    repetitions = (size_mb * 1024 * 1024) // len(block)
 
     with open(file_path, 'wb') as f:
-        for _ in range(file_size_mb):
-            f.write(os.urandom(chunk_size))  # Write 1MB of random bytes
-
-    print(f"10MB file created at: {file_path}")
+        for _ in range(repetitions):
+            f.write(block)
     return file_path
 
 def upload_file_to_s3(presigned_url, file_path):
@@ -30,20 +36,22 @@ def upload_file_to_s3(presigned_url, file_path):
     
     # Check if the upload was successful
     if response.status_code == 200:
-        print(f"File uploaded successfully to {presigned_url}")
+        return jsonify({'status': f'Upload successful {file_path}'})
     else:
-        print(f"Failed to upload file. Status code: {response.status_code}, Response: {response.text}")
+        return jsonify(f"Failed to upload file. Status code: {response.status_code}, Response: {response.text}")
 
 @app.route('/')
 def index():
     return render_template('eexample.html')
 
-@app.route('/s')
+import time
+@app.route('/s', methods=['POST'])
 def s():
-    filename = generate_10mb_file()
+    size_mb = int(reqf.get_json()['size_mb'])
+    filename = generate_file(size_mb)
     presigned_url = s3_client.generate_presigned_url('put_object',
                                                     Params={'Bucket': 'uploads', 'Key': filename},
                                                     ExpiresIn=3600)
-    upload_file_to_s3(presigned_url, filename)
+    return upload_file_to_s3(presigned_url, filename)
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
